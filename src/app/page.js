86709233c9346777cb161b3304fc855b5138df89
zaranachothani <chothanizarana45649@gmail.com>
2024-09-16 19:36:11 +0530

    Add good morning title

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,9 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>Hellooo, This is my first app to deploy</h1>
+      <h1>Hellooo, This is my first app to deploy<br/>
+        Good morning
+      </h1>
     </div>
   );
 }
